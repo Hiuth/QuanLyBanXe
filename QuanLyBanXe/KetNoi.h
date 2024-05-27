17b1b4b66b42ea3a7144bfa32e7e92a6006ac3f8
Hiuth <hiuth2004@gmail.com>
2024-05-28 00:34:35 +0700

    update ketnoi

---
+++ QuanLyBanXe/KetNoi.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <cppconn/driver.h> 
+#include <cppconn/exception.h> 
+#include <cppconn/statement.h> 
+#include <mysql_connection.h> 
+#include <mysql_driver.h> 
+#include <iostream>
+using namespace std;
+using namespace sql;
+using namespace mysql;
+class KetNoi
+{
+
+public:
+	const char* Password;
+	const char* UserName;
+	Connection* CheckDatabase();
+};
+
