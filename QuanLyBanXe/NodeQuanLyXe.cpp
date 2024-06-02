f756871b154fa42ea3a4d9a8bef259c6836336e6
Hiuth <Hiuth2004@gmail.com>
2024-06-02 18:23:06 +0700

    update

--- QuanLyBanXe/NodeQuanLyXe.cpp
+++ QuanLyBanXe/NodeQuanLyXe.cpp
@@ -1,1 +1,26 @@
 #include "NodeQuanLyXe.h"
+
+NodeQuanLyXe::NodeQuanLyXe(string MaXe, string TenXe, string GiaTien, string DongCo, string HopSo, string DanDong, string NamSanXuat, string TuiKhi, string HangXe, string TocDoToiDa, string KichThuoc, string KhoangSangGam, string PhanCapXe, string TonKho, string CongSuatToiDa)
+{
+	this->MaXe = MaXe;
+	this->TenXe = TenXe;
+	this->GiaTien = GiaTien;
+	this->DongCo = DongCo;
+	this->HopSo = HopSo;
+	this->DanDong = DanDong;
+	this->NamSanXuat = NamSanXuat;
+	this->TuiKhi = TuiKhi;
+	this->HangXe = HangXe;
+	this->TocDoToiDa = TocDoToiDa;
+	this->KichThuoc = KichThuoc;
+	this->KhoangSangGam = KhoangSangGam;
+	this->PhanCapXe = PhanCapXe;
+	this->TonKho = TonKho;
+	this->CongSuatToiDa = CongSuatToiDa;
+	this->pointer = NULL;
+}
+
+NodeQuanLyXe::~NodeQuanLyXe()
+{
+
+}
--- QuanLyBanXe/NodeQuanLyXe.h
+++ QuanLyBanXe/NodeQuanLyXe.h
@@ -20,10 +20,54 @@ private:
 	string PhanCapXe; // suv ,sedan, hatback, mpv,minicar
 	string TonKho;
 	string CongSuatToiDa;
+	NodeQuanLyXe* pointer;
 public:
-	NodeQuanLyXe(string, string, string, string, string, string, string, string, string, string, string, string, string, string,string);
+	NodeQuanLyXe(string, string, string, string, string, string, string, string, string, string, string, string, string, string, string);
 	virtual ~NodeQuanLyXe();
+
 	string LayTenXe() { return TenXe; }
-	void CaiTenXe(string val) { TenXe = val; }
+	void DatTenXe(string val) { TenXe = val; }
+
+	string LayMaXe() { return MaXe; }
+	void DatMaXe(string val) { MaXe = val; }
+
+	string LayGiaTien() { return GiaTien; }
+	void DatGiaTien(string val) { GiaTien = val; }
+
+	string LayLoaiDongCo() { return DongCo; }
+	void CaiDatLoaiDongCo(string val) { DongCo = val; }
+
+	string LayLoaiHopSo() { return HopSo; }
+	void CaiDatLoaiHopSo(string val) { HopSo = val; }
+
+	string LayLoaiDanDong() { return DanDong; }
+	void CaiDatLoaiDanDong(string val) { DanDong = val; }
+
+	string LayNamSanXuat() { return NamSanXuat; }
+	void CaiDatNamSanXuat(string val) { NamSanXuat = val; }
+
+	string LaySoTuiKhi() { return TuiKhi; }
+	void CaiDatSoTuiKhi(string val) { TuiKhi = val; }
+
+	string LayHangXe() { return HangXe; }
+	void CaiDatHangXe(string val) { HangXe = val; }
+
+	string LayTocDoToiDa() { return TocDoToiDa; }
+	void CaiDatTocDoToiDa(string val) {TocDoToiDa =val; }
+
+	string LayKichThuocXe() { return KichThuoc; }
+	void CaiDatKichThuocXe(string val) { KichThuoc = val; }
+
+	string LayKhoangSangGam() { return KhoangSangGam; }
+	void CaiDatKhoangSangGam(string val) {KhoangSangGam = val; }
+
+	string LayPhanCapXe() { return PhanCapXe; }
+	void CaiDatPhanCapXe(string val) { PhanCapXe = val; }
+	
+	string LayCongSuatToiDaCuaDongCo() { return CongSuatToiDa; }
+	void CaiDatCongSuatToiDaCuaDongCo(string val) { CongSuatToiDa = val; }
+
+	string LaySoLuongTonKho() { return TonKho; }
+	void CaiDatSoLuongTonKho(string val) { TonKho = val; }
 };
 
